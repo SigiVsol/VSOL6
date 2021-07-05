f40d33c00b2cc098b4a7221c67def8b97b42f169
Sigi <janssens.sigi@gmail.com>
2021-07-05 15:21:01 +0200

    Commit from OFFICE Dialog.inform / confirm / getString / getNumber

--- src/main/resources/js/app/Explorer.ts
+++ src/main/resources/js/app/Explorer.ts
@@ -2,6 +2,7 @@ import {Content} from "./Content.js";
 import {App} from "../App.js";
 import {Client} from "../model/Client.js";
 import {Patient} from "../model/Patient.js";
+import {Dialog} from "../popup/Dialog.js";
 
 export class Explorer extends Content {
 
@@ -9,7 +10,7 @@ export class Explorer extends Content {
         super(app, "divExplorer");
 
         $("#tglClients").click(() => {
-            this.app.setTab(null);
+            this.app.setTab("clients");
             this.app.pushHistory();
         });
 
@@ -23,6 +24,30 @@ export class Explorer extends Content {
             this.app.pushHistory();
         });
 
+        $("#btnTestInform").click(() => {
+            Dialog.inform("Information.");
+        });
+
+        $("#btnTestConfirm").click(() => {
+            Dialog.confirm("Are you sure?", () => {
+                console.log("I am sure");
+            }, () => {
+                console.log("I am not sure");
+            });
+        });
+
+        $("#btnTestGetString").click(() => {
+            Dialog.getString("What's your name?", "", (result) => {
+                console.log("Your name is " + result + ".");
+            });
+        });
+
+        $("#btnTestGetNumber").click(() => {
+            Dialog.getNumber("What's your birth year?", 2021, (result) => {
+
+                console.log("Your age is " + (2021 - result) + ".");
+            });
+        });
     }
 
     public fill() : void {
--- src/main/resources/js/popup/Dialog.js
+++ src/main/resources/js/popup/Dialog.js
@@ -1,6 +1,77 @@
 export class Dialog {
     static inform(text) {
-        // $("#divPopupLayer").css("display", "block");
-        console.log("INFORM: " + text);
+        Dialog.voidCallback = null;
+        Dialog.stringCallback = null;
+        Dialog.numberCallback = null;
+        Dialog.cancelCallback = null;
+        Dialog.show(text, null, false);
+    }
+    static confirm(text, yes, no = null) {
+        Dialog.voidCallback = yes;
+        Dialog.stringCallback = null;
+        Dialog.numberCallback = null;
+        Dialog.cancelCallback = no;
+        Dialog.show(text);
+    }
+    static getString(text, initial = "", ok, cancel = null) {
+        Dialog.voidCallback = null;
+        Dialog.stringCallback = ok;
+        Dialog.numberCallback = null;
+        Dialog.cancelCallback = cancel;
+        Dialog.show(text, initial);
+    }
+    static getNumber(text, initial = 0, ok, cancel = null) {
+        Dialog.voidCallback = null;
+        Dialog.stringCallback = null;
+        Dialog.numberCallback = ok;
+        Dialog.cancelCallback = cancel;
+        Dialog.show(text, String(initial));
+    }
+    static show(text, initial = null, cancelable = true) {
+        $("#divPopupLayer").css("display", "block");
+        $(".div-popup").css("display", "none");
+        $("#divPopupDialog").css("display", "inline-block");
+        $("#divPopupDialog p.popup-dialog-title").text(text);
+        $("#divPopupDialog .input-bar").css("display", initial == null ? "none" : "block");
+        const input = $("#divPopupDialog input");
+        if (initial != null) {
+            input.val(initial).select();
+            input.on("keyup", event => {
+                if (event.key === "Enter") {
+                    this.ok();
+                }
+                else if (event.key === "Escape") {
+                    this.cancel();
+                }
+            });
+        }
+        $("#divPopupDialog button.confirm").click(() => Dialog.ok());
+        $("#divPopupDialog button.close").css("display", cancelable ? "inline" : "none").click(() => Dialog.cancel());
+    }
+    static ok() {
+        const input = $("#divPopupDialog input");
+        if (Dialog.voidCallback != null)
+            Dialog.voidCallback();
+        else if (Dialog.stringCallback != null)
+            Dialog.stringCallback(String(input.val()));
+        else if (Dialog.numberCallback != null)
+            Dialog.numberCallback(Number(input.val()));
+        Dialog.hide();
+    }
+    static cancel() {
+        if (Dialog.cancelCallback != null)
+            Dialog.cancelCallback();
+        Dialog.hide();
+    }
+    static hide() {
+        $("#divPopupLayer").css("display", "none");
+        Dialog.voidCallback = null;
+        Dialog.stringCallback = null;
+        Dialog.numberCallback = null;
+        Dialog.cancelCallback = null;
     }
 }
+Dialog.voidCallback = null;
+Dialog.stringCallback = null;
+Dialog.numberCallback = null;
+Dialog.cancelCallback = null;
