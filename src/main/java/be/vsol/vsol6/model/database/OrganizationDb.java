0a8bcfac8e42f6157e5eabb5fd3a185e26a297c4
Juriën <jurien.beckers@veterinarysolutions.be>
2021-07-15 11:42:30 +0200

    Added SyncDb.deleteQueries & .updateRecords

--- src/main/java/be/vsol/vsol6/model/database/OrganizationDb.java
+++ src/main/java/be/vsol/vsol6/model/database/OrganizationDb.java
@@ -2,11 +2,14 @@ package be.vsol.vsol6.model.database;
 
 import be.vsol.database.connection.DbDriver;
 import be.vsol.database.model.DbTable;
+import be.vsol.util.Json;
 import be.vsol.vsol6.model.meta.Organization;
 import be.vsol.vsol6.model.organization.Client;
 import be.vsol.vsol6.model.organization.Patient;
 import be.vsol.vsol6.model.organization.Setting;
 import be.vsol.vsol6.model.organization.Study;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class OrganizationDb extends SyncDb {
 
@@ -22,6 +25,31 @@ public class OrganizationDb extends SyncDb {
         patients =  new DbTable<>(this, "patients", Patient::new);
         studies =  new DbTable<>(this, "studies", Study::new);
         settings =  new DbTable<>(this, "settings", Setting::new);
+    }
+
+    @Override public void updateRecords(JSONArray updates) {
+        for (int i = 0; i < updates.length(); i++) {
+            String tableName = updates.getJSONObject(i).getString("tableName");
+            JSONObject record = updates.getJSONObject(i).getJSONObject("record");
+            switch (tableName) {
+                case "clients" -> {
+                    Client client = Json.get(record, Client::new);
+                    this.getClients().save(client);
+                }
+                case "patients" -> {
+                    Patient patient = Json.get(record, Patient::new);
+                    this.getPatients().save(patient);
+                }
+                case "studies" -> {
+                    Study study = Json.get(record, Study::new);
+                    this.getStudies().save(study);
+                }
+                case "settings" -> {
+                    Setting setting = Json.get(record, Setting::new);
+                    this.getSettings().save(setting);
+                }
+            }
+        }
     }
 
     // Getters
